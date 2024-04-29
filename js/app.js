d1b544f8e443477e7cb209c1f4608ba0681a2e23
bluecat97 <theworldof1997@gmail.com>
2024-04-29 12:55:05 +0900

    0429

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /**
  *
  * @param {NodeList} elmts selected elements from HTML to add events
- * @param {string} event event type  e.g. 'click', 'mouseenter'...
+ * @param {string} event event type e.g. 'click', 'mouseenter'...
  * @param {Function} callback callback function to be executed when event is triggered
  */
 export function eventOnElmts(elmts, event, callback) {
@@ -9,3 +9,9 @@ export function eventOnElmts(elmts, event, callback) {
     elmt.addEventListener(event, callback);
   }
 }
+
+const a = document.querySelector('.current-weather .heading');
+
+a.addEventListener('click', () => {
+  a.classList.toggle('on');
+});
